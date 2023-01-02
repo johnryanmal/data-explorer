3948e6d0c50fca4ef496d057624b18d3287870af
johnryanmal <johnryanmal@gmail.com>
2023-01-02 14:04:14 -0800

    clean up

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'tty-prompt'
+require 'tty-tree'
 require 'awesome_print'
 
 require 'json'
@@ -18,7 +19,9 @@ loop do
 	mode = prompt.select("Explore with...", [:url, :data])
 	parser = prompt.select("Using...", parsers.keys)
 	parse = parsers.fetch(parser, lambda {|raw| raw})
+	data = nil
 
+	# load data
 	loop do 
 		case mode
 		when :url
@@ -35,7 +38,6 @@ loop do
 			raw = prompt.multiline("#{parser}:").join
 		end
 
-		data = nil
 		begin
 			data = parse.call(raw)
 		rescue
@@ -44,10 +46,19 @@ loop do
 			next
 		else
 			prompt.ok('Parsed data.')
-			ap data
+			break
 		end
-		break
 	end
+
+	# explore data
+	# TODO: make this
+	# TODO: sanitize strings in html content with str.dump
+	# TODO: display top level data, hide lower levels
+	ap data
+
+	#test = {a:1,b:2,c:3}
+	puts TTY::Tree.new(data).render
+
 	break
 end
 
