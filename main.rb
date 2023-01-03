09e51a13993052b9f356796ede472ade09385350
johnryanmal <johnryanmal@gmail.com>
2023-01-03 10:18:28 -0800

    make menu and cleanup ui

--- main.rb
+++ main.rb
@@ -50,6 +50,9 @@ loop do
 		end
 	end
 
+	prompt.keypress("Press any key to continue, resuming in :countdown...", timeout: 3)
+	system 'clear'
+
 	# explore data
 	# TODO: sanitize strings in html content with str.dump
 
@@ -65,33 +68,39 @@ loop do
 			nodes = curr.keys
 		end
 
+		header = "Level #{stack.length} - #{context}"
+
 		leaf = nodes.empty?
 		root = stack.empty?
 		opts = []
-		opts.push :select unless leaf
-		opts.push :back unless root
+		opts << :select unless leaf
+		opts << :back unless root
+		opts << :menu
 
-		action = prompt.select("Level #{stack.length} | #{context} <-", opts)
+		action = prompt.select("#{header} | Node", opts)
 		system 'clear'
 
 		case action
 		when :select
-			node = prompt.select("Level #{stack.length} | #{context} ->", nodes)
+			node = prompt.select("#{header} | Select", nodes)
 			system 'clear'
 			stack.push curr
 			curr = node
 		when :back
 			curr = stack.pop
+		when :menu
+			commands = [:resume, :new, :exit]
+			command = prompt.select("#{header} | Menu", commands)
+			system 'clear'
+
+			case command
+			when :resume
+				next
+			when :new
+				break
+			when :exit
+				exit
+			end
 		end
 	end
-
-	# ap data
-
-	# test = {a:1,b:2,c:3}
-	
-
-	break
 end
-
-
-#prompt.ask('Enter in a url')
