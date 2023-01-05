c1709555a45a32b0c220b419b6a4207f97af484f
johnryanmal <johnryanmal@gmail.com>
2023-01-04 21:29:02 -0800

    add select filtering

--- main.rb
+++ main.rb
@@ -47,8 +47,8 @@ prompt = TTY::Prompt.new
 pager = TTY::Pager.new
 
 loop do
-	mode = prompt.select("Explore with...", [:url, :data])
-	parser = prompt.select("Using...", parsers.keys)
+	mode = prompt.select("Explore with...", [:url, :data], filter: true)
+	parser = prompt.select("Using...", parsers.keys, filter: true)
 	parse = parsers.fetch(parser, lambda {|raw| raw})
 	data = nil
 
@@ -123,7 +123,7 @@ loop do
 		actions << :menu
 		action_opts = choices(options(actions))
 
-		action, action_cursor = prompt.select("#{header} | Node", action_opts, default: action_cursor, cycle: true)
+		action, action_cursor = prompt.select("#{header} | Node", action_opts, default: action_cursor, cycle: true, filter: true)
 		system 'clear'
 
 		case action
@@ -132,7 +132,7 @@ loop do
 		when :select
 			select_cursor = 1
 			loop do
-				node, select_cursor = prompt.select("#{header} | Select", node_opts, default: select_cursor, cycle: true, per_page: 10)
+				node, select_cursor = prompt.select("#{header} | Select", node_opts, default: select_cursor, cycle: true, filter: true, per_page: 10)
 				system 'clear'
 
 				view = (node.class == String)? node : node.ai
@@ -160,7 +160,7 @@ loop do
 					prompt.say(summary)
 					prompt.say("===\n")
 
-					command, view_cursor = prompt.select('Continue?', command_opts, default: view_cursor, cycle: true)
+					command, view_cursor = prompt.select('Continue?', command_opts, default: view_cursor, cycle: true, filter: true)
 					system 'clear'
 
 					if command == :view
@@ -185,7 +185,7 @@ loop do
 			curr = stack.pop
 		when :menu
 			commands = [:continue, :load, :exit]
-			command = prompt.select("#{header} | Menu", commands, cycle: true)
+			command = prompt.select("#{header} | Menu", commands, cycle: true, filter: true)
 			system 'clear'
 
 			case command
